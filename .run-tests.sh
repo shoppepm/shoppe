0dcb26dd7320a32afce883d7bd012a7651a8dff6
knuxify <knuxfanwin8.mail@gmail.com>
2019-07-22 21:05:38 +0200

    tests

---
+++ .run-tests.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+# This is a script used to automate testing.
+# It's used to verify that everything works correctly.
+
+set -ev
+
+./shoppe install boredbutton +fb
+
+ls ~/.config/shoppe
+ls ~/.config/shoppe/*
+
+./shoppe remove boredbutton
+./shoppe install boredbutton-git
+./shoppe remove boredbutton-git
+
+./shoppe update
+#./shoppe upgrade
